9181f08093232f248fec9d801663a6dc2eedaaed
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-03-04 10:44:45 +0100

    init project

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next"
+import { Epilogue } from "next/font/google"
+import "./globals.css"
+import Attribution from "./components/Attribution"
+
+const epilogue = Epilogue({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Intro Section with Dropdown Navigation",
+  description: "A Challenge from Frontend Mentor!",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+      </head>
+      <body
+        className={`${epilogue.className} bg-almost-white text-almost-black flex min-h-screen flex-col items-center`}
+      >
+        {children}
+        <Attribution />
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex h-full max-w-[90rem] flex-col">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+}
